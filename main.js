7a40a68af35ce581857cbcb5afe0d2923ae021e8
Vernon <jacksonv@oregonstate.edu>
2019-12-03 15:39:44 -0800

    Edit js file to perHour

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-//a memory location that never changes
 const BASE_URL = "https://zagster-service.herokuapp.com"
-const PI = 3.14159
 
-//constants are capitalized
-
-//jQuery command. Wait until web page loads call function
-//whose name is in parentheses
-//function call means rune the code
 $(greeter)
 
-//call funtion add (use it - make it run)
 add(2,3);
-//greeter is the function identifier. name is the functions
-//argument (info it needs to do its job)
+
 function add(num1, num2){
   answer = num1 + num2;
   console.log("The answer is: " + answer);
@@ -25,49 +16,65 @@ function greeter(name){
 greeter("Vernon's")
 
 
-var data = {"2016":[{"9":220},{"10":141},{"11":89},{"12":16}]}
-var year_list = data[2016]
-console.log('year list is' +year_list)
-
-console.log(year_list[0][9])
-console.log(year_list[1][10])
-console.log(year_list[2][11])
-console.log(year_list[3][12])
-
-$(updateView)
-
-let years = []
-let months2016 = []
-let months2017 = []
-let months2018 = []
-
- $(updateGraph)
+var data = []
 
 function updateView() {
   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
 
-  $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear),
+  $.when ($.getJSON(BASE_URL + "/rides/count/per_hour", perHour),
   ).then(updateChart);
 }
 
-function perYear(data) {
-  for (var index = 0, month = 9; index <= 3, month<=12; ++index, ++month){
-    months2016.push(data[2016] [index] [month])
-  }
-  console.log("2016 data by months is easy " + months2016)
-  
-  for (var index = 0, month = 9; index <= 3, month<=12; ++index, ++month){
-    months2016.push(data[2017] [index] [month])
-  }
-  console.log("2017 data by months is easy " + months2016)
-  
-  for (var index = 0, month = 9; index <= 3, month<=12; ++index, ++month){
-    months2016.push(data[2018] [index] [month])
-  }
-  console.log("2018 data by months is easy " + months2016)
+function perHour(data) {
+  var data =
+  console.log(data2016[0][429])
+  my_data.push(data2016[0][429])
+  console.log(data2016[1][231])
+  my_data.push(data2016[1][231])
+  console.log(data2016[2][130])
+  my_data.push(data2016[2][130])
+  console.log(data2016[3][85])
+  my_data.push(data2016[3][85])
+  console.log(data2016[4][42])
+  my_data.push(data2016[4][42])
+  console.log(data2016[5][1])
+  my_data.push(data2016[5][1])
+  console.log(data2016[7][1])
+  my_data.push(data2016[7][1])
+  console.log(data2016[8][17])
+  my_data.push(data2016[8][17])
+  console.log(data2016[9][16])
+  my_data.push(data2016[9][16])
+  console.log(data2016[10][795])
+  my_data.push(data2016[10][795])
+  console.log(data2016[11][425])
+  my_data.push(data2016[11][425])
+  console.log(data2016[12][921])
+  my_data.push(data2016[12][921])
+  console.log(data2016[13][846])
+  my_data.push(data2016[13][846])
+  console.log(data2016[14][1795])
+  my_data.push(data2016[14][1795])
+  console.log(data2016[15][1789])
+  my_data.push(data2016[15][1789])
+  console.log(data2016[16][2119])
+  my_data.push(data2016[16][2119])
+  console.log(data2016[17][1630])
+  my_data.push(data2016[17][1630])
+  console.log(data2016[18][1942])
+  my_data.push(data2016[18][1942])
+  console.log(data2016[19][1637])
+  my_data.push(data2016[19][1637])
+  console.log(data2016[20][1636])
+  my_data.push(data2016[20][1636])
+  console.log(data2016[21][1054])
+  my_data.push(data2016[21][1054])
+  console.log(data2016[22][843])
+  my_data.push(data2016[22][843])
+  console.log(data2016[23][710])
+  my_data.push(data2016[23][710])
 
 }
-
 function updateRideCount(data) {
    numberOfRides = data.count
   $("h2#rideCount").html(numberOfRides)
@@ -75,24 +82,21 @@ function updateRideCount(data) {
   console.log(numberOfRides)
 }
 
- function updateGraph() {
+ function updateChart() {
    var ctx = document.getElementById('myChart').getContext('2d');
  var chart = new Chart(ctx, {
-    // The type of chart we want to create
      type: 'line',
 
-//     // The data for our dataset
      data: {
-         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+         labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16','17', '18', '19', '20', '21', '22', '23'],
          datasets: [{
              label: 'Zagster Dataset',
              backgroundColor: '#da680f',
               borderColor: 'black',
-             data: [0, 10, 5, 2, 20, 30, 45]
+             data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
          }]
      },
 
-//     // Configuration options go here
      options: {}
  });
  }
