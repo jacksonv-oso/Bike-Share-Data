19da7a3127903e0255ea72fdb529ba0a04fa5c21
Vernon <jacksonv@oregonstate.edu>
2019-12-04 11:03:21 -0800

    Edit graph name js and html

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ function updateRideCount(data) {
      data: {
          labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16','17', '18', '19', '20', '21', '22', '23'],
          datasets: [{
-             label: 'Number of Hours Per Ride',
+             label: 'Number of Rides Per Hour',
              backgroundColor: '#da680f',
               borderColor: 'black',
              data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
